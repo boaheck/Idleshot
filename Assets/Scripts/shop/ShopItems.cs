0b2f937598190b8aad1a90b907e17d8cc4b0a885
Jessica Winters <jessica@winters.website>
2018-04-21 20:45:33 +0100

    Added ShopItems Structure and Asset

--- Assets/Scripts/shop/ShopItems.cs
+++ Assets/Scripts/shop/ShopItems.cs
@@ -2,7 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+[System.Serializable]
+public struct ShopFunctionValue
+{
+    public string key, value;
+}
+public enum ItemType
+{
+    ChangePlayerSpeed,
+    ChangeEnemyFrequency,
+    ChangePlayerBulletFrequency
+}
+[System.Serializable]
+public struct ShopItem
+{
+    public string id;
+    public string name;
+    public ItemType type;
+    [SerializeField]
+    public List<ShopFunctionValue> functionValues;
+}
+
+[CreateAssetMenu(fileName = "ShopItems", menuName = "Shop/Items", order = 1)]
 public class ShopItems : ScriptableObject
 {
-    public int item = 0;
+    public List<ShopItem> shopItems;
 }
