f7dd791ec5a90eece4e39ff40ee51699ae060ddb
Jessica Winters <jessica@winters.website>
2018-04-21 14:47:16 +0100

    Removed Debug Ray from shooting

--- Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -5,18 +5,12 @@ using UnityEngine;
 public class PlayerRotation : MonoBehaviour
 {
 
-    void Start()
-    {
-
-    }
-
     void FixedUpdate()
     {
         float distance = Vector3.Magnitude(transform.position - Camera.main.transform.position);
         Debug.Log(distance);
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance));
         Vector3 target = new Vector3(mousePos.x, transform.position.y, mousePos.z);
-        Debug.DrawLine(transform.position, target, Color.black, 0.1f);
 
         transform.LookAt(new Vector3(mousePos.x, transform.position.y, mousePos.z));
     }
