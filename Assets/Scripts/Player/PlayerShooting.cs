fa183730a01e174c1dc289457827a46ff624c134
Jessica Winters <jessica@winters.website>
2018-04-22 09:17:25 +0100

    Stop shooting when over UI element

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerShooting : MonoBehaviour {
 
@@ -18,31 +19,35 @@ public class PlayerShooting : MonoBehaviour {
 		scores = GameObject.Find ("Scores").GetComponent<ScoreSystem> ();
 	}
 
-	void Update () {
-		if (auto) {
-			if (Input.GetButtonDown ("Fire1")) {
-				Fire ();
-				timer = rate;
-			}else if(Input.GetButton("Fire1")){
-				if (timer <= 0) {
-					Fire ();
-					timer = rate;
-				} else {
-					timer -= Time.deltaTime;
-				}
-			}
-		} else {
-			if (timer <= 0) {
-				if (Input.GetButtonDown ("Fire1")) {
-					Fire ();
-					timer = rate;
-				}
-			} else {
-				timer -= Time.deltaTime;
-			}
-		}
+    void Update() {
+        bool onUI = EventSystem.current.IsPointerOverGameObject();
+        if (!onUI) {
 
-	}
+            if (auto) {
+                if (Input.GetButtonDown("Fire1")) {
+                    Fire();
+                    timer = rate;
+                } else if (Input.GetButton("Fire1")) {
+                    if (timer <= 0) {
+                        Fire();
+                        timer = rate;
+                    } else {
+                        timer -= Time.deltaTime;
+                    }
+                }
+            } else {
+                if (timer <= 0) {
+                    if (Input.GetButtonDown("Fire1")) {
+                        Fire();
+                        timer = rate;
+                    }
+                } else {
+                    timer -= Time.deltaTime;
+                }
+            }
+        }
+
+    }
 
 	void Fire(){
 		Instantiate (projectile, transform.position + (transform.rotation * shotOffset), transform.rotation);
