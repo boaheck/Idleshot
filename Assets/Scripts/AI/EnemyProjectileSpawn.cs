257fa963b753da8047781c309b8103bda26f2caf
boaheck <breogan.hackett2@mail.dcu.ie>
2018-04-22 23:24:00 +0200

    add enemy shooting system and enemy model

---
+++ Assets/Scripts/AI/EnemyProjectileSpawn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectileSpawn : MonoBehaviour {
+
+	[HideInInspector] public EnemyShooting shooter;
+	[HideInInspector] public float timeToShoot;
+	[SerializeField]float targetScale = 0.2f;
+	[SerializeField]float minScale = 0.01f;
+
+	public GameObject projectile;
+
+	float timer;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (shooter.shooting) {
+			if (timer > 0) {
+				timer -= Time.deltaTime;
+			} else {
+				Instantiate (projectile, transform.position, transform.rotation);
+				timer += timeToShoot;
+			}
+		}
+		transform.localScale = Vector3.one * Mathf.Lerp (minScale, targetScale, 1 - (timer / timeToShoot));
+	}
+
+	public void setTimer(float newTime){
+		timer = newTime;
+	}
+}
