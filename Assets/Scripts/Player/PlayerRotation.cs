1a37a2cae2e374fa7a0321db682d01af363203f5
Jessica Winters <jessica@winters.website>
2018-04-22 09:07:12 +0100

    Fix formatting

--- Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/Scripts/Player/PlayerRotation.cs
@@ -2,36 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRotation : MonoBehaviour
-{
+public class PlayerRotation : MonoBehaviour {
 
-	bool mousemode = false;
+    bool mousemode = false;
 
-	void Update(){
-		if (mousemode) {
-			if (Mathf.Abs (Input.GetAxis ("AimX")) > 0.1 || Mathf.Abs (Input.GetAxis ("AimY")) > 0.1) {
-				mousemode = false;
-			}
-		} else {
-			if (Mathf.Abs (Input.GetAxis ("Mouse X")) > 0.1 || Mathf.Abs (Input.GetAxis ("Mouse Y")) > 0.1) {
-				mousemode = true;
-			}
-		}
-	}
+    void Update() {
+        if (mousemode) {
+            if (Mathf.Abs(Input.GetAxis("AimX")) > 0.1 || Mathf.Abs(Input.GetAxis("AimY")) > 0.1) {
+                mousemode = false;
+            }
+        } else {
+            if (Mathf.Abs(Input.GetAxis("Mouse X")) > 0.1 || Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.1) {
+                mousemode = true;
+            }
+        }
+    }
 
-    void FixedUpdate(){
-		if (mousemode) {
-			float distance = Vector3.Magnitude (transform.position - Camera.main.transform.position);
-			Debug.Log (distance);
-			Vector3 mousePos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance));
-			Vector3 target = new Vector3 (mousePos.x, transform.position.y, mousePos.z);
+    void FixedUpdate() {
+        if (mousemode) {
+            float distance = Vector3.Magnitude(transform.position - Camera.main.transform.position);
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance));
+            Vector3 target = new Vector3(mousePos.x, transform.position.y, mousePos.z);
 
-			transform.LookAt (new Vector3 (mousePos.x, transform.position.y, mousePos.z));
-		} else {
-			Vector3 lookdir = new Vector3 (Input.GetAxis ("AimX"), 0, Input.GetAxis ("AimY"));
-			if (lookdir.magnitude > 0.2) {
-				transform.forward = lookdir.normalized;
-			}
-		}
+            transform.LookAt(new Vector3(mousePos.x, transform.position.y, mousePos.z));
+        } else {
+            Vector3 lookdir = new Vector3(Input.GetAxis("AimX"), 0, Input.GetAxis("AimY"));
+            if (lookdir.magnitude > 0.2) {
+                transform.forward = lookdir.normalized;
+            }
+        }
     }
 }
