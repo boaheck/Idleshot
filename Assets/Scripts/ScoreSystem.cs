fdfb21cad314310c970b48dcb0012342d2cea979
boaheck <breogan.hackett2@mail.dcu.ie>
2018-04-23 14:27:17 +0200

    Add enemy health@

--- Assets/Scripts/ScoreSystem.cs
+++ Assets/Scripts/ScoreSystem.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class ScoreSystem : MonoBehaviour {
 
 	int shells = 0;
+	int jelly = 0;
 	public Text shellDisplay;
+	public Text jellyDisplay;
 
 	public int GetShells(){
 		return shells;
@@ -31,6 +33,28 @@ public class ScoreSystem : MonoBehaviour {
 
 	void updateDisplay(){
 		shellDisplay.text = "Shells: " + shells;
+		jellyDisplay.text = "Jelly: " + jelly;
+	}
+
+	public int GetJelly(){
+		return shells;
+	}
+
+	public void AddJelly(){
+		AddShells (1);
+	}
+
+	public void AddJelly(int amt){
+		shells += amt;
+		if (shells < 0) {
+			shells = 0;
+		}
+		updateDisplay ();
+	}
+
+	public void ClearJelly(){
+		shells = 0;
+		updateDisplay ();
 	}
 
 }
